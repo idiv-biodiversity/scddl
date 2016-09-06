a7e0b3fa230437da1fad113094a86c7d9ad419f0
Christian Krause <christian.krause@idiv.de>
2016-09-06 11:25:49 +0200

    syslog instead of echo
    - replaces all relevant echo commands with logger -t commands
    - moves the download to its own function
    - renames the functions

--- blastdl.sh
+++ blastdl.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# TODO replace echo with `logger -t $(dirname $0) message`
-
 # ------------------------------------------------------------------------------
 # configuration, arguments
 # ------------------------------------------------------------------------------
@@ -25,11 +23,30 @@ BLAST_DB_DIR=$2
 BLAST_DB_DL_DIR=$(mktemp -d --tmpdir=$BLAST_DB_DIR .blastdl-XXXXXXXXXX)
 trap 'rm -rf $BLAST_DB_DL_DIR' EXIT INT TERM
 
+BLAST_DB_DATE=$(date +%F)
+
 # ------------------------------------------------------------------------------
 # application functions
 # ------------------------------------------------------------------------------
 
-function update_metadata_file {
+function log.info {
+  logger -p user.info -t blastdl "$@"
+}
+
+function log.err {
+  logger -p user.err -t blastdl "$@"
+}
+
+function blastdl.download {
+  # download all md5s first, then download all tarballs
+  cat << EOF | lftp ftp://ftp.ncbi.nlm.nih.gov
+set net:socket-buffer 33554432
+mirror -r -P 8 -i "^$BLAST_DB_DATASET\.[0-9]+\.tar\.gz\.md5$" /blast/db $BLAST_DB_DL_DIR
+mirror -r -P 8 -i "^$BLAST_DB_DATASET\.[0-9]+\.tar\.gz$"      /blast/db $BLAST_DB_DL_DIR
+EOF
+}
+
+function blastdl.update.metadata {
   case $BLAST_DB_DATASET in
     nr)
       BLAST_DB_METADATA_FILE=$BLAST_DB_DATASET.pal
@@ -39,7 +56,7 @@ function update_metadata_file {
       BLAST_DB_METADATA_FILE=$BLAST_DB_DATASET.nal
       ;;
     *)
-      echo "[blastdl] [$(date)] unknown database" >&2
+      log.err "unknown database $BLAST_DB_DATASET"
       return 1
       ;;
   esac
@@ -52,32 +69,21 @@ function update_metadata_file {
 # application
 # ------------------------------------------------------------------------------
 
-# download all md5s first, then download all tarballs
-echo "[blastdl] [$(date)] starting download ..." >&2
-cat << EOF | lftp ftp://ftp.ncbi.nlm.nih.gov
-set net:socket-buffer 33554432
-mirror -r -P 8 -i "^$BLAST_DB_DATASET\.[0-9]+\.tar\.gz\.md5$" /blast/db $BLAST_DB_DL_DIR
-mirror -r -P 8 -i "^$BLAST_DB_DATASET\.[0-9]+\.tar\.gz$"      /blast/db $BLAST_DB_DL_DIR
-EOF
-echo "[blastdl] [$(date)] done" >&2
-
-pushd $BLAST_DB_DL_DIR &> /dev/null
-
-BLAST_DB_DATE=$(date +%F)
-
-echo "[blastdl] [$(date)] checking md5 ..." >&2 &&
+log.info "starting download of $BLAST_DB_DATASET database ..." &&
+blastdl.download &&
+log.info "... download finished, checking md5 ..." &&
+pushd $BLAST_DB_DL_DIR &> /dev/null &&
 md5sum --check --quiet *.md5 &&
-echo "[blastdl] [$(date)] done, extracting ..." >&2 &&
+log.info "... md5 success, extracting ..." &&
 for i in $BLAST_DB_DATASET.*.tar.gz ; do
   tar xzfo $i || exit 1
 done &&
-echo "[blastdl] [$(date)] done, tagging with date and moving ..." >&2 &&
-update_metadata_file &&
+log.info "... extracting finished, tagging with date and moving ..." &&
+blastdl.update.metadata &&
 for i in $BLAST_DB_DATASET.* ; do
   mv -n $i $BLAST_DB_DIR/${i/$BLAST_DB_DATASET/$BLAST_DB_DATASET-$BLAST_DB_DATE} || exit 1
 done &&
-echo "[blastdl] [$(date)] done, setting read only ..." >&2 &&
+log.info "... moving done, setting read only ..." &&
 chmod 444 $BLAST_DB_DIR/$BLAST_DB_DATASET-$BLAST_DB_DATE* &&
-echo "[blastdl] [$(date)] done." >&2
-
+log.info "... set read only, done." &&
 popd &> /dev/null
