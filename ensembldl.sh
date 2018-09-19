6e898e7767750dc0469f58bcc26fa1b17441686c
M Bernt <m.bernt@ufz.de>
2018-09-19 11:42:58 +0200

    ensebldl: added test without any chechsum

--- ensembldl.sh
+++ ensembldl.sh
@@ -13,7 +13,7 @@ version=$(git describe --always --long --dirty 2> /dev/null) ||
   version="0.1.0"
 
 # get utilities
-# shellcheck disable=SC1091
+# shellcheck source=util.sh
 source "$(dirname "$0")"/util.sh
 
 # -----------------------------------------------------------------------------
