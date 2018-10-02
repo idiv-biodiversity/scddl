9bd1675d2f589715bfbf33dd590193aab15a46a2
M Bernt <m.bernt@ufz.de>
2018-10-02 18:43:30 +0200

    ucscdl: make use of md5sum_verbosity + improve shellcheck annotation

--- ucscdl.sh
+++ ucscdl.sh
@@ -13,7 +13,7 @@ version=$(git describe --always --long --dirty 2> /dev/null) ||
   version="0.1.0"
 
 # get utilities
-# shellcheck source=util.sh
+# shellcheck disable=SC1091
 source "$(dirname "$0")"/util.sh
 
 # -----------------------------------------------------------------------------
@@ -287,7 +287,7 @@ do
       cat "$hash"
       rm "$hash"
     done |
-    md5sum -c --quiet ||
+    md5sum -c $md5sum_verbosity ||
     bailout 'verification error'
 
   log.verbose "extracting files"
