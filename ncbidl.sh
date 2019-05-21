4bc0c238b7ea313526424b9c45ef619bdda9d5a8
Christian Krause <christian.krause@idiv.de>
2019-05-21 16:34:11 +0200

    bumps version

--- ncbidl.sh
+++ ncbidl.sh
@@ -10,7 +10,7 @@ set \
 app=$(basename "$0" .sh)
 
 version=$(git describe --always --long --dirty 2> /dev/null) ||
-  version="0.2.0"
+  version="0.3.0"
 
 # get utilities
 # shellcheck source=util.sh
